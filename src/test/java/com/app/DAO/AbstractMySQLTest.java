dc5f626f23999b0e1fbfade028d89f08dad68144
Thien phuc <thienphuc484@gmail.com>
2025-07-22 10:31:28 +0700

    test(SubCommentDao): add integrate test

--- src/test/java/com/app/DAO/AbstractMySQLTest.java
+++ src/test/java/com/app/DAO/AbstractMySQLTest.java
@@ -171,6 +171,18 @@ public abstract class AbstractMySQLTest {
             + "FOREIGN KEY (board_id) REFERENCES boards(id) ON DELETE CASCADE,"
             + "FOREIGN KEY (pin_id) REFERENCES pins(id) ON DELETE CASCADE"
             + ")");
+    jdbcTemplate.execute(
+        "CREATE TABLE IF NOT EXISTS sub_comments ("
+            + "id INT AUTO_INCREMENT PRIMARY KEY,"
+            + "content TEXT,"
+            + "user_id INT,"
+            + "comment_id INT,"
+            + "media_id INT,"
+            + "create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,"
+            + "FOREIGN KEY (comment_id) REFERENCES comments(id) ON DELETE CASCADE,"
+            + "FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,"
+            + "FOREIGN KEY (media_id) REFERENCES media(id) ON DELETE CASCADE"
+            + ")");
 
     jdbcTemplate.update(
         "INSERT IGNORE INTO media (id, url, media_type) VALUES (?, ?, ?)", 1, "url", "IMAGE");
