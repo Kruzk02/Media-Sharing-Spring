81bb81f8def028a91fbc7cb98a29a7ba52b43c9a
Thien phuc <thienphuc484@gmail.com>
2026-04-10 10:25:03 +0700

    Delete HashtagController

--- src/main/java/com/app/module/hashtag/api/HashtagController.java
+++
@@ -1,9 +0,0 @@
-package com.app.module.hashtag.api;
-
-import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("/api/hashtags")
-@AllArgsConstructor
-public class HashtagController {}
